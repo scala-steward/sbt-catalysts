abca27deaed040e9ae5c2c1b5f814b4d27173e62
Scala Steward <me@scala-steward.org>
2022-04-01 20:35:02 +0200

    Update shapeless to 2.3.9

--- src/main/scala/org/typelevel/package.scala
+++ src/main/scala/org/typelevel/package.scala
@@ -33,7 +33,7 @@ object `package` {
     .add(name = "scalacheck",       version = "1.15.4", org = "org.scalacheck")
     .add(name = "scalatest",        version = "3.2.11",  org = "org.scalatest")
     .add(name = "scodec",           version = "1.11.9", org = "org.scodec", "scodec-core")
-    .add(name = "shapeless",        version = "2.3.8",  org = "com.chuusai")
+    .add(name = "shapeless",        version = "2.3.9",  org = "com.chuusai")
     .add(name = "simulacrum",       version = "1.0.1", org = "org.typelevel")
     .add(name = "specs2",           version = "4.14.1",  org = "org.specs2", "specs2-core", "specs2-scalacheck", "specs2-mock")
     .add(name = "scalac"   ,        version = "2.13.8")
